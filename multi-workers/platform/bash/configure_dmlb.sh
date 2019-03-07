f26829dd2b55f09377b954e219fa77cf6c785869
IamTao <itamtao@gmail.com>
2019-03-07 13:57:33 +0100

    add the env setup for distributed training.

---
+++ multi-workers/platform/bash/configure_dmlb.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Create and activate env  --  Install pytorch from conda
+echo 'Creating env and Installing pip'
+conda create -y -n dmlb-env python=3.6.3 anaconda pip
+source activate dmlb-env
+
+# Get the pip and python and conda from anaconda
+pip_in_env=`which pip`
+python_in_env=`which python`
+current_env=/home/$HOME/.conda/envs/dmlb-env/
+
+echo "USING PYTHON: $python_in_env"
+echo "USING PIP: $pip_in_env"
+echo "USING CONDA IN: $conda_in_env"
+echo "INSTALLING IN: $current_env"
+
+# Install requirements (need cd otherwise sudo is denied...)
+echo 'Installing requirements'
+cd dmlb
+sudo $pip_in_env install -r requirement.txt
+
+mpirun --version &> /dev/null
+if [ $? != 0 ]; then
+   echo "MPI is not installed"
+   exit
+fi
+
+cuda_aware=`ompi_info --parsable --all | grep mpi_built_with_cuda_support:value`
+if [ ${cuda_aware##*:} != "true" ]; then
+   echo "MPI is not cuda aware"
+   exit
+fi
+
+# Install pytorch from source
+export CMAKE_PREFIX_PATH="$(dirname $(which conda))/../"
+
+sudo /opt/anaconda3/bin/conda install -y jpeg numpy pyyaml mkl mkl-include setuptools cmake cffi --prefix=$current_env
+sudo /opt/anaconda3/bin/conda install -y -c soumith magma-cuda90 --prefix=$current_env
+sudo /opt/anaconda3/bin/conda install -y protobuf --prefix=$current_env
+sudo /opt/anaconda3/bin/conda install -y -c anaconda cython --prefix=$current_env
+
+# install left packages.
+$pip_in_env install opencv-python
+
+# install torch and torch-vision.
+git clone --recursive https://github.com/pytorch/pytorch
+cd pytorch && \
+    git submodule update --init && \
+    TORCH_CUDA_ARCH_LIST="3.5 3.7 5.2 6.0 6.1 7.0+PTX" TORCH_NVCC_FLAGS="-Xfatbin -compress-all" \
+    $pip_in_env install -v . && cd .. && rm -rf pytorch/
+
+# Install torchvision from source
+if [[ "$1" ==  "--no-vision" ]];
+   then
+    exit 0
+fi
+
+# install torchvision
+git clone https://github.com/pytorch/vision.git
+cd vision && $python_in_env setup.py install && cd .. && rm -rf vision/
